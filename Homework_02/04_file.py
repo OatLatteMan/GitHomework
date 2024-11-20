f68aff603645ac9051c8a2f645b0d07a4e6f6bda
Dmitrii Panov <15minstan@gmail.com>
2024-11-20 13:09:42 +0100

    Slight modification

--- Homework_02/04_file.py
+++ Homework_02/04_file.py
@@ -1,5 +1,5 @@
 def append_file():
-    with open ('GitHomework/Homework_02/test.txt', mode='a', encoding='utf-8') as file:
+    with open ('GitHomework/Homework_02/04_file.txt', mode='a', encoding='utf-8') as file:
         file.write(input('Tell me about your today\'s mood?'))
         file.write('\n')
 
